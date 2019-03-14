daf03390a25078d746715ca88a68c29f9ed6e09a
esmyers <emyers@usgs.gov>
2019-03-14 12:59:11 -0500

    hardcoded sites in, markercluster, multiple webcam sites handled

--- main.js
+++ main.js
@@ -5,22 +5,83 @@ The application and data are intended as proof of concept only and are not to be
 */
 
 var siteInfo;
+var xmlSites;
             // initialize the map
             $(document).ready(function(){
-                //json 
-                $.getJSON('webcam_site_info.json', function(data){
-                    //console.log(data);
-                    siteInfo = data;
-                });
+               $.get('sites.xml', function (xml) {
+                    xmlSites = xml
+                    console.log('DEV NOTE: this version using local xml file for latlng lookup');
+                    startComparison();
+                }); 
+                function startComparison(){
+                    var testMarkers = {};
+                    $.getJSON('webcam_site_info.json', function (data) {
+                        console.log(data);
+                        siteInfo = data;
+                        var count = 0;
+                        $(siteInfo).each(function (index, site) {
+                            //console.log(site);
+                            var siteID = site.SiteId;
+                            var siteName = site.siteDesc;
+                            var customPopup = '<div><b>Site Number: </b><a href="https://waterdata.usgs.gov/nwis/uv?site_no=' + site.SiteId + '&Parameter_cd=00065,00060" target="_blank">' +
+                               site.SiteId + '</a><div><b>Description: </b>' + site.siteDesc + '</div></div></br><a href='
+                                + site.timelapseRoot + site.timelapseLarge +
+                                ' target="_blank"><img width="200" src=' + site.thumbnail + ' ></a><div>click image to open full-size video</div></br><div><b>Video Folder: </b><a href=' +
+                                site.timelapseFolder + ' target="_blank">Index of videos and images</a></div><div><b>Frames Gallery: </b><a href=' +
+                               site.timelapseFolder + 'frame_gallery/ target="_blank">Last 100 Frames</a></div></br>';
+                            var latlngArr = findSite(siteID);
+
+                            //only continue if valid latlng found
+                            if (latlngArr[0] != "error") {
+                                testMarkers[siteID] = L.circleMarker(latlngArr, { fillColor: "#d11010", color: "#000", weight: 0, fillOpacity: 0.6, radius: 8 }).bindPopup(customPopup);
+                                sitesLayer.addLayer(testMarkers[siteID])
+                                count++;
+                            }
+
+
+                        });
+                        console.log("count = ", count);
+
+                        function findSite(siteId) {
+                            var latlng = [];
+                            var fakelat = 0;
+                            $(xmlSites).find('site').each(function () {
+                                if ($(this).attr('sno') == siteId) {
+                                    latlng.push($(this).attr('lat'));
+                                    latlng.push($(this).attr('lng'));
+                                }
+                            });
+                            //handle sites that aren't in the NWIS sites.xml
+                            if (latlng.length > 0) {
+                                return latlng;
+                            } else {
+                                console.log('unable to find site in NWIS return: ', siteId)
+                                latlng.push('error');
+                                return latlng;
+                            }
+
+                        }
+                    });//end getJSON
+                }//end startComparison
+                
+
+               
+                    
+                    
+
+                
+                
+
+               
                 
                 //send 2 requests because NWIS can only handle 100 at a time.
                 var xmlurl1 = 'https://nwis.waterservices.usgs.gov/nwis/site/?format=mapper,1.0&sites=01161280,01426500,01437500,01465500,01480000,01481000,03093000,03339000,04024430,04025500,040734605,040734605,04073462,04079000,04079000,04080000,04080000,04081000,04085108,040851385,04086000,04086000,04086600,04087050,0408708567,0408708572,040870860,04087087,04087119,04087120,04087204,04087214,04087220,04087233,04087234,04087240,04087257,0414826504,0414826504,0414826504,041572269,041572269,041572269,04182000,04183000,04183038,04183500,04183979,04185318,04186500,04188100,04190000,04191444,04192500,04226000,04236000,05287890,05289800,05331833,05333500,05341752,05342000,05342000,05356000,05356500,05359500,05365550,05366800,05367500,05369500,05370000,05379400,05382200,05382267,05382284,05399500,05400760,05401050,05402000,05407000,054187201,054187202,05424057,05427085,05427530,05427718,05427880,05427910,05427927,05427930,05427943,054279465,05427948,05427965,05428500,05429500,05429500,05429700,05430150';
                 var xmlurl2 = 'https://nwis.waterservices.usgs.gov/nwis/site/?format=mapper,1.0&sites=05430175,05430500,05433000,05434500,05435943,05435950,05436500,06342500,08361000,13289220,334716104175400,405051083391201,405051083391201,405051083391201,405051083391201,411228084541701,411228084541701,411228084541701,411229084541101,411229084541101,411229084541101,411229084541102,411229084541102,411229084541102,413612087201001,424129077495501,424129077495501,424129077495501,424129077495502,424131077495801,424131077495801,424131077495801,424131077495801,424131077495802,424421077495301,424421077495301,424421077495301,424421077495401,424421077495401,424421077495401,425520078535601,425520078535601,425543078535001,425931087570201,425931087570201,430356089183502,430356089183502,430402089183501,430402089183501,430402089183501,430402089183501,430402089183501,430402089183501,430402089183501,430402089183501,430532089315601,430532089315601,430532089315601,430532089315602,430532089315602,433821090063801,433821090063801,433821090063801,433939090050501,433939090050501,433939090050501,441520088045001,441520088045001,441546088082001,441546088082001,441546088082001,441624088045601,441624088045601,441624088045601,441715088115801,441715088115801,441715088115801,441715088115802,441715088115803,441715088115803,442114088085701,442114088085701,442114088085701,442119088085501,442119088085501,442119088085501,442155087330001,442155087330001,442155087330001,442405087414401,442405087414401,442405087414401,443535087345401,451021089064901,451021089064901,451021089064901,451207089041801,451207089041801,451207089041801,463741090521301';
                 var webcamMarkers = {};
                 var siteCount = 0;
-
                 
-                function getData(url){
+                
+               /*  function getData(url){
                    
                     $.ajax({
                         method: "get",
@@ -39,7 +100,6 @@ var siteInfo;
                             var lat = $(this).attr('lat');
                             var lng = $(this).attr('lng');
                             webcamMarkers[siteID] = L.circleMarker([lat, lng], { fillColor: "red", color:"#000", weight:0, fillOpacity: 0.6, radius: 8 }).bindPopup(customPopup);
-
                             webcamMarkers[siteID].data = { siteName: siteName, siteCode: siteID };
                             webcamMarkers[siteID].data.parameters = {};
 
@@ -69,7 +129,8 @@ var siteInfo;
 
                 
                 getData(xmlurl1);
-                getData(xmlurl2);
+                getData(xmlurl2); */
+
   
             }); //end self-invoking jquery func
             
@@ -87,11 +148,14 @@ var siteInfo;
                 zoom: 5,
                 layers: [topo]
             });
+            var mcg = L.markerClusterGroup({showCoverageOnHover: false, maxClusterRadius:.1, spiderfyDistanceMultiplier: 2}).addTo(map);
 
-            var sitesLayer = L.featureGroup().addTo(map);
+            var sitesLayer = L.featureGroup.subGroup(mcg).addTo(map);
+            /* var sitesLayer = L.markerClusterGroup({showCoverageOnHover: false, maxClusterRadius: .1, spiderfyDistanceMultiplier: 2});
+            map.addLayer(sitesLayer); */
 
             //leaflet tilelayer of NWIS sites
-            var swActTileLayer = L.tileLayer("https://nwismapper.s3.amazonaws.com/sw_act/{z}/{y}/{x}.png", {zIndex:999,maxZoom:8,errorTileUrl:"https://nwismapper.s3.amazonaws.com/blank.png"}).addTo(map);
+            var swActTileLayer = L.tileLayer("https://nwismapper.s3.amazonaws.com/sw_act/{z}/{y}/{x}.png", {zIndex:999,maxZoom:8,errorTileUrl:"https://nwismapper.s3.amazonaws.com/blank.png"})
             
             //objects for layer controls
             var overlayLayers = {   
